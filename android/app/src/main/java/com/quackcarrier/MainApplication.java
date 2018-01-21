031a2068c9bcd3236162e871846b7b5ccdabed1f
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2018-01-21 01:07:57 -0200

    Lottie on android

--- android/app/src/main/java/com/quackcarrier/MainApplication.java
+++ android/app/src/main/java/com/quackcarrier/MainApplication.java
@@ -3,6 +3,7 @@ package com.quackcarrier;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.lottie.LottiePackage;
 import com.microsoft.codepush.react.CodePush;
 import com.reactnativenavigation.NavigationReactPackage;
 import com.facebook.react.ReactNativeHost;
@@ -31,6 +32,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new LottiePackage(),
             new CodePush(null, getApplicationContext(), BuildConfig.DEBUG),
             new NavigationReactPackage()
       );
